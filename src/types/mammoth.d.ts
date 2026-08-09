e0f5bbc87f8f95e8ad03722897954c0d7d3af8bb
tontakankeawpang <tontakankeawpang321@gmail.com>
2026-08-09 09:25:50 +0700

    Updates

--- src/types/mammoth.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'mammoth' {
-  export interface MammothResult {
-    value: string;
-    messages: any[];
-  }
-  export function extractRawText(options: { arrayBuffer: ArrayBuffer }): Promise<MammothResult>;
-  export function convertToHtml(options: { arrayBuffer: ArrayBuffer }): Promise<MammothResult>;
-}
